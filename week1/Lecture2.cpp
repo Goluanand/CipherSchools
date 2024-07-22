cea7074c63b3ddb9be14568fa9009e6424942a1d
Goluanand <goluanandrathuaur@gmail.com>
2024-07-22 23:58:40 +0530

    intro

---
+++ week1/Lecture2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>    // This line means header
+
+using namespace std;  // std make sure thst cout is a part of "iostream"
+
+int main()     // we have written a main "function"
+{
+    cout << "Hello World!";
+
+    return 0;
+}
